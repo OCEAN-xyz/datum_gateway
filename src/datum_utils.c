21e5de7ca40e8fefae45286c9dd425e43b48d02f
Luke Dashjr <luke-jr+git@utopios.org>
2025-04-16 03:35:07 +0000

    utils: Refactor strncpy_workerchars and strncpy_uachars for readability

--- src/datum_utils.c
+++ src/datum_utils.c
@@ -555,7 +555,7 @@ bool strncpy_workerchars(char *out, const char *in, size_t maxlen) {
 	out[0] = 0;
 	while((in[i] != 0) && (maxlen > 1)) {
 		c = in[i];
-		if ((isalnum(c)) || (c == '.') || (c == '-') || (c == '_') || (c == '=') || (c == '@') || (c == ',')) {
+		if (isalnum(c) || strchr(".-_=@,", c)) {
 			out[j] = c;
 			j++;
 			maxlen--;
@@ -582,7 +582,7 @@ bool strncpy_uachars(char *out, const char *in, size_t maxlen) {
 	out[0] = 0;
 	while((in[i] != 0) && (maxlen > 1)) {
 		c = in[i];
-		if ((isalnum(c)) || (c == '.') || (c == '-') || (c == '_') || (c == '=') || (c == '@') || (c == ',') || (c == ' ') || (c == '|') || (c == '/') || (c == '|') || (c == ':') || (c == '<') || (c == '>') || (c == '\'') || (c == ';')) {
+		if (isalnum(c) || strchr(".-_=@, |/:<>';", c)) {
 			out[j] = c;
 			j++;
 			maxlen--;
